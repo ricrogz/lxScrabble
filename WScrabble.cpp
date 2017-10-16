da233c3b62d9a06d4f0849966992264060bdc900
invik <ricrogz@gmail.com>
2017-10-17 00:17:12 +0200

    Included all headers into main cpp file

--- WScrabble.cpp
+++ WScrabble.cpp
@@ -3,7 +3,22 @@
 // WScrabble.cpp : définit le point d'entrée pour l'application console.
 //
 
-#include "stdafx.h"
+#pragma once
+
+#define WIN32_LEAN_AND_MEAN		// Exclure les en-têtes Windows rarement utilisés
+#include <stdio.h>
+#include <tchar.h>
+#pragma warning(disable:4710)
+#pragma warning(push,3)
+#pragma warning(disable:4702)
+#pragma warning(disable:4701)
+#include <iostream>
+#include <fstream>
+#pragma warning(pop)
+#include <conio.h>
+#include <time.h>
+#include <winsock.h>
+
 
 #if defined(_DEBUG) && 0
 #define OFFLINE
