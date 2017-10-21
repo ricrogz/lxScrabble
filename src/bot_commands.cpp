812c7e199994b80aa0da1056a4fb732ca219d2ba
invik <ricrogz@gmail.com>
2017-10-21 02:41:14 +0200

    make commands in help bold

--- src/bot_commands.cpp
+++ src/bot_commands.cpp
@@ -9,23 +9,23 @@
 void help_cmd(const string &dest, bool is_owner) {
 
     string user_commands[] = {
-            "!start: Start a game (works only when the bot is stopped).",
-            "!score: Show your current score.",
-            "!score <nick>: Show <nick>'s current score.",
-            "!top10: Show this week's 10 best scores.",
-            "!top: Alias of : !top10: .",
-            "!top10year: Show this year's 10 best scores.",
-            "!top3: Show this week's 3 best scores.",
-            "!top3year: Show this year's 3 best scores.",
-            "!r: Show again the letters for the ongoing game."
+            "\002!start\002: Start a game (works only when the bot is stopped).",
+            "\002!score\002: Show your current score.",
+            "\002!score <nick>\002: Show <nick>'s current score.",
+            "\002!top10\002: Show this week's 10 best scores.",
+            "\002!top\002: Alias of \002!top10\002.",
+            "\002!top10year\002: Show this year's 10 best scores.",
+            "\002!top3\002: Show this week's 3 best scores.",
+            "\002!top3year\002: Show this year's 3 best scores.",
+            "\002!r\002: Show again the letters for the ongoing game."
     };
 
-    string stop_cmd = "!stop: Stop the bot.";
+    string stop_cmd = "\002!stop\002: Stop the bot.";
 
     string owner_commands[] = {
-            "!newweek: Reset the weekly scores, and start a new week.",
-            "!quit " + bot_nick + ": Quit. WARNING: this will stop the bot, and you will have to start the program again.",
-            "!op: get op'ed by the bot (the bot must have 'op' itself)."
+            "\002!newweek\002: Reset the weekly scores, and start a new week.",
+            "\002!quit " + bot_nick + "\002: Quit. WARNING: this will stop the bot, and you will have to start the program again.",
+            "\002!op\002: get op'ed by the bot (the bot must have 'op' itself)."
     };
 
     // User commands
