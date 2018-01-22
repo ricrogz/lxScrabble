a79e0bdd3ca8a773657dfc9d420bad183981eef3
invik <ricrogz@gmail.com>
2018-01-22 22:37:38 +0100

    Use local time in logs.

--- src/mimics.h
+++ src/mimics.h
@@ -36,7 +36,7 @@ inline void log(const char *message, std::ostream& stream) {
     char timestamp[25];
     struct tm *sTm;
     time_t now = time(nullptr);
-    sTm = gmtime (&now);
+    sTm = localtime(&now);
     strftime(timestamp, 25, "%Y-%m-%d %H:%M:%S --- ", sTm);
     stream << timestamp << message << std::endl;
 }
