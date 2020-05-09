49d7f10ea912318fb129485952b0cabe6a6aaeaa
Ric <ricrogz@gmail.com>
2020-05-09 12:23:37 -0400

    refactor bot commands

--- src/bot_commands.cpp
+++ src/bot_commands.cpp
@@ -1,6 +1,7 @@
 //
 // Created by invik on 20/10/17.
 //
+#include <vector>
 
 #include "bot_commands.hpp"
 
@@ -9,7 +10,7 @@
 void help_cmd(const std::string& dest, bool is_owner)
 {
 
-    std::string user_commands[] = {
+    const std::vector<std::string> user_commands = {
         "\002!start\002: Start a game (works only when the bot is stopped).",
         "\002!score\002: Show your current score.",
         "\002!score <nick>\002: Show <nick>'s current score.",
@@ -22,7 +23,7 @@ void help_cmd(const std::string& dest, bool is_owner)
 
     std::string stop_cmd = "\002!stop\002: Stop the bot.";
 
-    std::string owner_commands[] = {
+    const std::vector<std::string> owner_commands = {
         "\002!newweek\002: Reset the weekly scores, and start a new week.",
         "\002!quit " + public_nick +
             "\002: Quit. WARNING: this will stop the bot, and you will have to "
